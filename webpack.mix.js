5d9557d323cd2c1e67c85edef42b30bd2d3ebec7
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-11-30 12:31:47 +0100

    Add custom CSS and update theme layout for improved styling

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require("laravel-mix");
 mix
     .js("./src/js/custom.js", "assets")
-    sass("./src/scss/base.scss", "assets")
-    .sass("./src/scss/custom.scss", "assets")
+    .sass("./src/scss/css-file.scss", "assets")
     .setPublicPath("");
